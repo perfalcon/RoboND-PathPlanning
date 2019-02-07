02e88eb0eda8948d96206be4f1cdc0766311701f
perfalcon <balaveer@gmail.com>
2019-02-07 10:21:34 -0500

    1
    1

---
+++ catkin_ws/src/ShellScripts/test_slam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /home/workspace/catkin_ws; 
+source /home/workspace/catkin_ws/devel/setup.bash;
+
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+
+xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
+
+
