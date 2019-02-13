c261b15809cf02d7a7a5f14da82cfce9273aa233
perfalcon <balaveer@gmail.com>
2019-02-12 21:15:33 -0500

    1
    1

---
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/home_service_robot.world " &
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/wall_my_map.yaml 3d_sensor:=kinect " &
+sleep 5
+xterm  -e  " rosrun rviz rviz -d /home/workspace/catkin_ws/src/RvizConfig/navigation.rviz " &
+sleep 5
+xterm  -e  " rosrun add_markers add_markers " &
+sleep 5
+xterm  -e  " rosrun pick_objects pick_objects "
--- catkin_ws/src/ShellScripts/test_navigation.sh
+++ catkin_ws/src/ShellScripts/test_navigation.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/home_service_robot.world " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/wall_my_map.yaml 3d_sensor:=kinect " &
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
-sleep 5
-xterm  -e  " rosrun wall_follower wall_follower " &
