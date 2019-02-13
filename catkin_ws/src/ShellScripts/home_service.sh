7fe444abfd5a99b86c01cfda52ff96fef101ad45
perfalcon <balaveer@gmail.com>
2019-02-12 21:23:00 -0500

    1
    1

--- catkin_ws/src/ShellScripts/home_service.sh
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -4,6 +4,9 @@ sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/wall_my_map.yaml 3d_sensor:=kinect " &
 sleep 5
 xterm  -e  " rosrun rviz rviz -d /home/workspace/catkin_ws/src/RvizConfig/navigation.rviz " &
+# update view_navigation.launch to use the /home/workspace/catkin_ws/src/RvizConfig/navigation.rviz, then
+# xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+
 sleep 5
 xterm  -e  " rosrun add_markers add_markers " &
 sleep 5
